6a17a43bb08df06b83e3b02e2dc1a3be17ae38d7
OtakeYuto <yuto.programming@gmail.com>
2023-09-11 00:59:44 +0900

    Refactor GET /pet endpoint to use Prisma for data retrieval

--- routes/pet/getAllPets.js
+++ routes/pet/getAllPets.js
@@ -1,89 +1,61 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const sqlite3 = require("sqlite3");
 
-// Connect to database
-const dbFile = "database.sqlite3";
-const dbConnect = () => {
-  // Create a new database connection
-  const db = new sqlite3.Database(dbFile);
-
-  // Enable foreign key constraints for this connection
-  db.run("PRAGMA foreign_keys=ON");
-
-  return db;
-};
+// Initialize Prisma Client instance
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // Middleware applied
 router.use(express.json());
 
 // Get all pets
-router.get("/", (req, res) => {
-  const db = dbConnect();
-
-  // SELECT pets's information
-  const selectPets = `
-    SELECT
-       p.id petID,
-       p.category_id categoryID,
-       c.name categoryName,
-       p.name petName,
-       p.status petStatus
-    FROM pets p 
-    JOIN categories c ON p.category_id = c.id;`;
-
-  // SELECT tags's information using pet_id
-  const selectTags = `
-    SELECT 
-       t.id tagID,
-       t.name tagName
-    FROM tags t
-    JOIN pet_tags pt ON pt.tag_id = t.id
-    WHERE pt.pet_id = ? `;
-
-  // SELECT pet_photos's information using pet_id
-  const selectPhots = `
-    SELECT 
-      photo_url photoURL
-    FROM pet_photos 
-    WHERE pet_id = ? `;
-
-  // Create base response
-  db.all(selectPets, (err, pets) => {
-    const formattedResponse = pets.map((pet) => ({
-      id: pet.petID,
-      category: { id: pet.categoryID, name: pet.categoryName },
-      name: pet.petName,
-      status: pet.petStatus,
-    }));
-
-    // Add pet.tags to the response
-    const promises_getTags = formattedResponse.map((pet) => {
-      return new Promise((resolve, reject) => {
-        db.all(selectTags, [pet.id], (err, tags) => {
-          pet.tags = tags.map((tag) => ({ id: tag.tagID, name: tag.tagName }));
-          resolve();
-        });
-      });
-    });
-
-    // Add pet.photoUrls to the response
-    const promises_getPhotUrl = formattedResponse.map((pet) => {
-      return new Promise((resolve, reject) => {
-        db.all(selectPhots, [pet.id], (err, photos) => {
-          pet.photoUrls = photos.map((photo) => photo.photoURL);
-          resolve();
-        });
-      });
-    });
-
-    // After all promises, respond and close DB.
-    Promise.all([...promises_getTags, ...promises_getPhotUrl]).then(() => {
-      res.status(200).json(formattedResponse);
-      db.close();
-    });
+router.get("/", async (req, res) => {
+  const pets = await prisma.pets.findMany({
+    select: {
+      id: true,
+      category: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      name: true,
+      pet_photos: {
+        select: {
+          photo_url: true,
+        },
+      },
+      pet_tags: {
+        select: {
+          tag: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+      status: true,
+    },
   });
+
+  // formatted response
+  const formattedPets = pets.map((pet) => {
+    return {
+      id: pet.id,
+      category: pet.category,
+      name: pet.name,
+      photoUrls: pet.pet_photos.map((photo) => photo.photo_url),
+      tags: pet.pet_tags.map((pet_tag) => ({
+        id: pet_tag.tag.id,
+        name: pet_tag.tag.name,
+      })),
+      status: pet.status,
+    };
+  });
+
+  return res.json(formattedPets);
 });
 
 module.exports = router;
