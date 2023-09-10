79ab5a09e286fab6d3dedd6ad2c82c031be78fa5
OtakeYuto <yuto.programming@gmail.com>
2023-09-11 02:36:38 +0900

    Refactor DELETE /pet/:id endpoint to use Prisma

--- routes/pet/deleteAPetByPetId.js
+++ routes/pet/deleteAPetByPetId.js
@@ -1,62 +1,57 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const sqlite3 = require("sqlite3");
 
-// Connect to database
-const dbFile = "database.sqlite3";
-const dbConnect = () => {
-  // Create a new database connection
-  const db = new sqlite3.Database(dbFile);
-
-  // Enable foreign key constraints for this connection
-  db.run("PRAGMA foreign_keys=ON");
-
-  return db;
-};
+// Initialize Prisma Client instance
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // Middleware applied
 router.use(express.json());
 
 // Delete a pet by petId
 router.delete("/:id", async (req, res) => {
-  // Get request
-  const id = req.params.id;
+  try {
+    // Get request
+    const id = req.params.id;
 
-  // Check request's id
-  let idError = false;
-  const num = Number(id);
-  if (isNaN(num)) {
-    return res.status(400).json({ error: "Invalid ID supplied" });
-    idError = true;
-  }
-
-  if (!idError) {
-    const db = dbConnect();
-    // Check requested data exists in the table
-    let foundPet = false;
-    const selectRequestPet = `SELECT COUNT(*) count FROM pets WHERE id = ?;`;
-    const promise_checkRequest = new Promise((resolve, reject) => {
-      db.get(selectRequestPet, [id], (err, pet) => {
-        if (pet.count === 0) {
-          foundPet = true;
-        }
-        resolve();
+    // Check request's id
+    let idError = false;
+    const num = Number(id);
+    if (isNaN(num)) {
+      return res.status(400).send({
+        code: 400,
+        type: "Bad Request",
+        message: "Invalid ID supplied",
       });
+    }
+    // Check requested data exists in the table
+    const petCount = await prisma.pets.count({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (petCount === 0) {
+      return res.status(404).send({
+        code: 404,
+        type: "Not Found",
+        message: "Pet not found",
+      });
+    }
+
+    const deletedPet = await prisma.pets.delete({
+      where: {
+        id: Number(id),
+      },
     });
 
-    await promise_checkRequest.then(() => {
-      if (foundPet) {
-        return res.status(404).json({ error: "Pet not found" });
-      } else {
-        // Execute the SQL
-        const deletePet = `DELETE FROM pets WHERE id = ? ;`;
-        db.run(deletePet, [id]);
-
-        res.status(200).json({ message: `Deleted petId: ${id}` });
-      }
+    res.status(200).send({ message: `Deleted petId: ${id}` });
+  } catch (error) {
+    return res.status(500).send({
+      code: 500,
+      type: "Internal Server Error",
+      message: error.message,
     });
-    db.close();
   }
 });
 
